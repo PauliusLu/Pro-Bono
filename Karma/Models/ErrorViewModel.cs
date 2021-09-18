dac7fb50a2fa5767072263ea3db81c010fd0d78e
Beater <77802186+Beateris@users.noreply.github.com>
2021-09-18 19:48:12 +0300

    Add project files.

---
+++ Karma/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Karma.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
