ffa128af02f1f516161820ddbfcd5b91600121a5
Viktorija <baikauskaitev@gmail.com>
2021-10-03 15:38:55 +0300

    Create a sqlite database and add Post table (#9)
    * Include EntityFrameWorkCore packages Design and Sqlite.
    * Change database context to sqlite option in Startup.cs
    * Remove old migrations.
    * Add new migrations to initialize Post table in Karma.db
    * Change connection string to Karma.db
    * Change several properties to required in Post.cs
    * Remove TODOs from Dialog views.

--- Karma/Data/KarmaContext.cs
+++ Karma/Data/KarmaContext.cs
@@ -15,5 +15,22 @@ namespace Karma.Data
         }
 
         public DbSet<Karma.Models.Post> Post { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Id);
+                entity.Property(e => e.UserId).IsRequired();
+                entity.Property(e => e.IsDonation).IsRequired();
+                entity.Property(e => e.Date).IsRequired();
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.ItemType).HasMaxLength(30).IsRequired();
+                entity.Property(e => e.Description).HasMaxLength(120).IsRequired();
+                entity.Property(e => e.ImagePath);
+                entity.Property(e => e.IsVisible).IsRequired();
+            });
+        }
     }
 }
--- Karma/Migrations/20211002162958_Initial.Designer.cs
+++ Karma/Migrations/20211002162958_Initial.Designer.cs
@@ -3,58 +3,55 @@ using System;
 using Karma.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Karma.Migrations
 {
     [DbContext(typeof(KarmaContext))]
-    [Migration("20210926175731_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20211002162958_Initial")]
+    partial class Initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("Relational:MaxIdentifierLength", 128)
-                .HasAnnotation("ProductVersion", "5.0.9")
-                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                .HasAnnotation("ProductVersion", "5.0.10");
 
             modelBuilder.Entity("Karma.Models.Post", b =>
                 {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
-                        .HasColumnType("int")
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+                        .HasColumnType("INTEGER");
 
                     b.Property<DateTime>("Date")
-                        .HasColumnType("datetime2");
+                        .HasColumnType("TEXT");
 
                     b.Property<string>("Description")
                         .IsRequired()
                         .HasMaxLength(120)
-                        .HasColumnType("nvarchar(120)");
+                        .HasColumnType("TEXT");
 
                     b.Property<string>("ImagePath")
-                        .HasColumnType("nvarchar(max)");
+                        .HasColumnType("TEXT");
 
                     b.Property<bool>("IsDonation")
-                        .HasColumnType("bit");
+                        .HasColumnType("INTEGER");
 
                     b.Property<bool>("IsVisible")
-                        .HasColumnType("bit");
+                        .HasColumnType("INTEGER");
 
                     b.Property<int>("ItemType")
-                        .HasColumnType("int");
+                        .HasMaxLength(30)
+                        .HasColumnType("INTEGER");
 
                     b.Property<string>("Title")
                         .IsRequired()
                         .HasMaxLength(30)
-                        .HasColumnType("nvarchar(30)");
+                        .HasColumnType("TEXT");
 
                     b.Property<int>("UserId")
-                        .HasColumnType("int");
+                        .HasColumnType("INTEGER");
 
                     b.HasKey("Id");
 
