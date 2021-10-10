a1fc517635a071576d32583cf98d961d7d8e9164
baikauskaite <baikauskaitev@gmail.com>
2021-10-10 23:55:26 +0300

    Implement username/email login

--- Karma/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Karma/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Text.RegularExpressions;
 
 namespace Karma.Areas.Identity.Pages.Account
 {
@@ -44,8 +45,8 @@ namespace Karma.Areas.Identity.Pages.Account
         public class InputModel
         {
             [Required]
-            [Display(Name = "User name")]
-            public string UserName { get; set; }
+            [Display(Name = "Username or Email")]
+            public string UserNameOrEmail { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
@@ -75,14 +76,61 @@ namespace Karma.Areas.Identity.Pages.Account
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
+            bool isLoginWithEmail = false;
 
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
-        
+
+            // Determining whether user logins with username or email
+            if (Input.UserNameOrEmail.IndexOf('@') > -1)
+            {
+                // Validate email format
+                string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                                @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+                Regex regex = new Regex(emailRegex);
+                if (!regex.IsMatch(Input.UserNameOrEmail))
+                {
+                    ModelState.AddModelError("UserNameOrEmail", "Email is not valid");
+                }
+                else
+                {
+                    isLoginWithEmail = true;
+                }
+            }
+            else
+            {
+                // Validate username format, it shouldn't have @
+                // TODO: specific error for @
+                string userNameRegex = @"^[a-zA-Z0-9]*$";
+                Regex regex = new Regex(userNameRegex);
+                if (!regex.IsMatch(Input.UserNameOrEmail))
+                {
+                    ModelState.AddModelError("UserNameOrEmail", "Username is not valid");
+                }
+            }
+
             if (ModelState.IsValid)
             {
+                // If email, get user's username
+                var userName = Input.UserNameOrEmail;
+                if (isLoginWithEmail)
+                {
+                    var user = await _userManager.FindByEmailAsync(userName);
+                    if (user == null)
+                    {
+                        ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                        return Page();
+                    }
+                    else
+                    {
+                        userName = user.UserName;
+                    }
+                }
+
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-                var result = await _signInManager.PasswordSignInAsync(Input.UserName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
+                var result = await _signInManager.PasswordSignInAsync(userName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
