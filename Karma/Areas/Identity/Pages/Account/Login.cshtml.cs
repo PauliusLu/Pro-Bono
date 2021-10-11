782619281fc6e3b4d644f75ca323500a7821204e
baikauskaite <baikauskaitev@gmail.com>
2021-10-11 20:39:49 +0300

    Fix lost line during merge conflict solving

--- Karma/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Karma/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -130,6 +130,7 @@ namespace Karma.Areas.Identity.Pages.Account
 
                 // This doesn't count login failures towards account lockout
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(userName, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User logged in.");
