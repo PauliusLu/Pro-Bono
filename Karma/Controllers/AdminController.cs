3aed58840fa1ee7e41014d3fd9272dce5f555b16
baikauskaite <baikauskaitev@gmail.com>
2021-10-22 18:09:43 +0300

    Implement Admin pages for managing user roles
    * Create AdminController.cs
    * Add methods for creating roles
    * Add methods for managing user roles

---
+++ Karma/Controllers/AdminController.cs
@@ -0,0 +1,118 @@
+﻿using Karma.Data;
+using Karma.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karma.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        private readonly UserManager<User> userManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(Role role)
+        {
+            var roleExists = await roleManager.RoleExistsAsync(role.Name);
+
+            if (!roleExists)
+            {
+                var result = await roleManager.CreateAsync(new IdentityRole(role.Name));
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ManageUserRoles(string userId)
+        {
+            ViewBag.userId = userId;
+
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                ViewBag.errorMessage = $"User with Id = {userId} cannot be found";
+                return NotFound();
+            }
+
+            var model = new List<UserRolesViewModel>();
+
+            foreach(var role in roleManager.Roles)
+            {
+                var userRolesViewModel = new UserRolesViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name
+                };
+                
+                if (await userManager.IsInRoleAsync(user, role.Name))
+                {
+                    userRolesViewModel.IsSelected = true;
+                }
+                else
+                {
+                    userRolesViewModel.IsSelected = false;
+                }
+
+                model.Add(userRolesViewModel);
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(List<UserRolesViewModel> model, string userId)
+        {
+            var user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                ViewBag.errorMessage = $"User with Id = {userId} cannot be found";
+                return NotFound();
+            }
+
+            var userRoles = await userManager.GetRolesAsync(user);
+            var result = await userManager.RemoveFromRolesAsync(user, userRoles);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot remove existing user roles");
+                return View(model);
+            }
+
+            result = await userManager.AddToRolesAsync(user, 
+                model.Where(r => r.IsSelected).Select(r => r.RoleName));
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError("", "Cannot add selected roles to the user");
+                return View(model);
+            }
+
+            return View(model);
+        }
+    }
+}
