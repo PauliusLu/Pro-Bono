fb13228e1c7d296cf895c67264b67e880ac715e2
baikauskaite <baikauskaitev@gmail.com>
2021-11-13 22:06:55 +0200

    Implement ILogger in PostsController.cs
    * Create LogEvents.cs to define event IDs
    * Override default set of logging providers in Startup.cs

---
+++ Karma/Models/LogEvents.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karma.Models
+{
+    public class LogEvents
+    {
+        // Post events
+        public const int GetPost = 1001;
+        public const int CreatePost = 1002;
+        public const int EditPost = 1003;
+        public const int DeletePost = 1004;
+        public const int ChangeState = 1005;
+
+        // User events
+        public const int UserNotAuthenticated = 2001;
+
+        // File events
+        public const int AddImage = 3001;
+    }
+}
