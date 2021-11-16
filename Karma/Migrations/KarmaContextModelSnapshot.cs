92532964aa6516c8ca844606f1871eb6fc307f1f
baikauskaite <baikauskaitev@gmail.com>
2021-11-16 15:58:33 +0200

    Fix identation and database

--- Karma/Migrations/KarmaContextModelSnapshot.cs
+++ Karma/Migrations/KarmaContextModelSnapshot.cs
@@ -14,7 +14,7 @@ namespace Karma.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "5.0.11");
+                .HasAnnotation("ProductVersion", "5.0.12");
 
             modelBuilder.Entity("Karma.Models.Charity", b =>
                 {
@@ -106,6 +106,16 @@ namespace Karma.Migrations
                         .HasColumnType("INTEGER");
 
                     b.Property<string>("CreatorId")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<bool>("IsSeenByCreator")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<bool>("IsSeenByPostUser")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("PostUserId")
                         .IsRequired()
                         .HasColumnType("TEXT");
 
