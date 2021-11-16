bea5989276a86c0e8b2638f539792c9b8a775028
baikauskaite <baikauskaitev@gmail.com>
2021-11-16 09:33:02 +0200

    Create report migration and update database

---
+++ Karma/Migrations/20211116072624_CreateReport.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Karma.Migrations
+{
+    public partial class CreateReport : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Report",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PostId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PostOwnerId = table.Column<string>(type: "TEXT", nullable: false),
+                    ReporterId = table.Column<string>(type: "TEXT", nullable: false),
+                    ReportState = table.Column<int>(type: "INTEGER", nullable: false),
+                    ReportMessage = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Report", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Report");
+        }
+    }
+}
