b49592160ab7c7838a24ce38b8d488b1bfc6ff76
Tomas Blažauskas <blazauskas.tomas@gmail.com>
2021-12-03 15:25:03 +0200

    Unfinnished edit.

--- KarmaTests/Controllers/ProfileControllerTests.cs
+++ KarmaTests/Controllers/ProfileControllerTests.cs
@@ -11,6 +11,7 @@ using Karma.Data;
 using Moq;
 using Autofac.Extras.Moq;
 using Karma.Models;
+using Microsoft.AspNetCore.Hosting;
 
 namespace Karma.Controllers.Tests
 {
@@ -18,20 +19,37 @@ namespace Karma.Controllers.Tests
     public class ProfileControllerTests
     {
 
+        private readonly ProfileController _sut;
+        private readonly Mock<KarmaContext> _contextMock = new Mock<KarmaContext>();
+        private readonly Mock<IWebHostEnvironment> _webHostEnvironmentMock = new Mock<IWebHostEnvironment>();
+
+        public ProfileControllerTests()
+            {
+            _sut = new ProfileController(_contextMock.Object, _webHostEnvironmentMock.Object);
+            }
 
         [Fact]
-        public async Task Index_ReturnsAViewResult_()
+        public async Task Edit_()
         {
+            
+
             // Arrange
-            var ProfileMockAgent = new Mock<KarmaContext>();
-            var controller = new ProfileController(ProfileMockAgent.Object);
-            controller.RouteData();
+            var id = Guid.NewGuid();
+            var userDto = new User
+            {
+                UserName = "Labas",
+                Name = "l",
+                Surname = "aa"
+            };
+            
+            var user = await _sut.Edit(id.ToString());
+            _contextMock.Setup(x => x.Edit(id)).
+                ReturnAsync(userDto);
             // Act
-
-
+            var userTest = await _sut.Edit(id.ToString());
             // Assert
 
-
+            Assert.Equal();
 
         }
 
