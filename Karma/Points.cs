374dfc217da6105962db93fae832108338f14e1d
ArmintasP <75906786+ArmintasP@users.noreply.github.com>
2021-12-06 19:02:47 +0200

    Add popover and remove magic number

---
+++ Karma/Points.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Karma
+{
+    public class Points
+    {
+        public static int TradedPostValue => 3;
+    }
+}
